412d71bd9d475dac72b2f16ab9fb5445fda88fe4
l80 <l80@devadmin.com>
2016-03-24 13:25:57 +0300

    PUM-145

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -163,7 +163,8 @@ class Controller {
             $this->app->response->headers->set('Content-Disposition', 'attachment; filename="'.$outputName.'"; modification-date="'.date('r').'";');
 
         $flag = false;
-            foreach($csv->fetch(PDO::FETCH_ASSOC) as $row) {
+            foreach($row = $csv->fetch(PDO::FETCH_ASSOC)) {
+
                 if(!$flag) {
                     fputcsv($output, array_keys($row));
                     $flag = true;
